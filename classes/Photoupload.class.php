3d9f2439955890bdd0d5ff112cdd7289796bba14
Riho Noormets <noorriho@greeny.cs.tlu.ee>
2017-11-27 09:16:13 +0200

    Profiil

---
+++ classes/Photoupload.class.php
@@ -0,0 +1,119 @@
+<?php
+	class Photoupload{
+		/*public $publicTest;
+		private $privateTest;*/
+		private $tempName;
+		private $fileType;
+		private $textToImage;
+		private $myTempImage;
+		private $myImage;
+		private $marginRight;
+		private $marginBottom;
+		public  $exifToImage;
+		
+		
+		function __construct($fileToUpload, $fileType){
+			$this->tempName = $fileToUpload;
+			$this->imageFileType = $fileType;
+			$this->marginRight = 10;
+			$this->marginBottom= 10;
+			/*$this->publicTest = "Täitsa avalik";
+			$this->privateTest= $x;*/
+		}//function__construct lõppeb
+		public function readExif(){
+			//loeme EXIF infot, millal pilt tehti
+			@$exif = exif_read_data($this->tempName, "ANY_TAG", 0, true);
+			//var_dump($exif);
+			if(!empty($exif["DateTimeOriginal"])){
+				$this->textToImage = "Pilt tehti: " .$exif["DateTimeOriginal"];
+			} else {
+				$this->textToImage = "Pildistamise aeg teadmata!";
+			}
+		}//function readExif lõppeb
+		private function createImage(){
+			if($this->imageFileType == "jpg" or $imageFileType == "jpeg"){
+				$this->myTempImage = imagecreatefromjpeg($this->tempName);
+			}
+			if($this->imageFileType == "png"){
+				$this->myTempImage = imagecreatefrompng($this->tempName);
+			}
+			if($this->imageFileType == "gif"){
+				$this->myTempImage = imagecreatefromgif($this->tempName);
+			}			
+		}//Function createImage lõppeb
+		
+		public function resizeImage($width, $height){
+			//küsime originaalsuurust
+			$this->createImage();
+			$imageWidth = imagesx($this->myTempImage);
+			$imageHeight = imagesy($this->myTempImage);
+			$sizeRatio = 1;
+			if($imageWidth > $imageHeight){
+				$sizeRatio = $imageWidth / $width;
+			} else {
+				$sizeRatio = $imageHeight / $height;
+			}
+			$this->myImage = $this->resize_image($this->myTempImage, $imageWidth, $imageHeight, round($imageWidth / $sizeRatio), round($imageHeight / $sizeRatio));
+		}//resizeImage lõppeb
+		
+		private function resize_image($image, $origW, $origH, $w, $h){
+			$dst = imagecreatetruecolor($w, $h);
+			//asendame musta värvi pikslid läbipaistvatega, et säilitada vajadusel png piltide läbipaistvus
+			imagesavealpha($dst, true);
+			$transcolor = imagecolorallocatealpha($dst, 0, 0, 0, 127);
+			imagefill($dst, 0, 0, $transcolor);
+			imagecopyresampled($dst, $image,0, 0, 0, 0, $w, $h, $origW, $origH);
+			return $dst; 
+		}//function resize image lõppeb
+			/*vesimärgi lisamine
+		public function addWatermark(){
+			$stamp = imagecreatefrompng("../graphics/hmv_logo.png");
+			$stampWidth = imagesx($stamp);
+			$stampHeight = imagesy($stamp);
+			$stampPosX = imagesx($this->myImage) - $stampWidth - $this->marginRight;
+			$stampPosY = imagesy($this->myImage) - $stampHeight - $this->marginBottom;
+			imageCopy($this->myImage, $stamp, $stampPosX, $stampPosY, 0, 0, $stampWidth, $stampHeight);
+		}//function addWatermark lõppeb
+		
+		public function addTextWatermark($text){
+		//lisame ka teksti vesimärgina
+			$textColor = imagecolorallocatealpha($this->myImage, 150, 150, 150, 50);
+			//RGBA alpha 0 -127
+			imagettftext($this->myImage, 20, 0, 10, 25, $textColor, "../graphics/ARIAL.TTF", $text);
+		}//function addTextWatermark lõppeb*/
+		
+		
+		public function savePhoto($directory, $fileName){
+			$target_file= $directory .$fileName;
+			$notice="";
+			if($this->imageFileType == "jpg" or $this->imageFileType == "jpeg"){
+				if(imagejpeg($this->myImage, $target_file, 90)){
+					$notice .="true";
+				} else {
+					$notice .= "false";
+				}
+			}
+			if($this->imageFileType == "png"){
+				if(imagepng($this->myImage, $target_file, 6)){
+					$notice = "true";
+				} else {
+					$notice .= "false";
+				}
+			}
+			if($this->imageFileType == "gif"){
+				if(imagegif($this->myImage, $target_file)){
+					$notice = "true";
+				} else {
+					$notice .= "false";
+				}
+			}
+		}//function savephoto lõppeb
+		
+		public function clearImages(){
+			imagedestroy($this->myImage);
+			imagedestroy($this->myTempImage);
+		}//function clearImages lõppeb		
+		
+	}//class lõppeb
+
+?>
---
+++ profiiliredigeerimine.php
@@ -0,0 +1,146 @@
+<?php
+require("functions.php");
+	$notice = "";
+	$allIdeas = "";
+	
+	
+//Liidan klassi
+	require("classes/Photoupload.class.php");
+	
+	function convertImage($originalImage, $outputImage, $quality)
+	{// jpg, png, gif or bmp?
+    $exploded = explode('.',$originalImage);
+    $ext = $exploded[count($exploded) - 1]; 
+
+    if (preg_match('/jpg|jpeg/i',$ext))
+        $imageTmp=imagecreatefromjpeg($originalImage);
+    else if (preg_match('/png/i',$ext))
+        $imageTmp=imagecreatefrompng($originalImage);
+    else if (preg_match('/gif/i',$ext))
+        $imageTmp=imagecreatefromgif($originalImage);
+    else if (preg_match('/bmp/i',$ext))
+        $imageTmp=imagecreatefrombmp($originalImage);
+    else
+        return 0;
+
+    // quality is a value from 0 (worst) to 100 (best)
+    imagejpeg($imageTmp, $outputImage, $quality);
+    imagedestroy($imageTmp);
+
+    return 1;
+	}
+	
+	//kui pole sisseloginud, siis sisselogimise lehele
+	if(!isset($_SESSION["userId"])){
+		header("Location: login.php");
+		exit();
+	}
+	
+	//kui logib välja
+	if (isset($_GET["logout"])){
+		//lõpetame sessiooni
+		session_destroy();
+		header("Location: login.php");
+	}
+	
+	//Algab foto laadimise osa
+	$target_dir = "../uploads/";
+	$target_file;
+	$uploadOk = 1;
+	$imageFileType;
+	$maxWidth = 600;
+	$maxHeight = 400;
+	$marginBottom = 10;
+	$marginRight = 10;
+	
+	//Kas on pildi failitüüp
+	if(isset($_POST["submit"])) {
+		
+		//kas mingi fail valiti
+		if(!empty($_FILES["fileToUpload"]["name"])){
+			
+			$imageFileType = strtolower(pathinfo(basename($_FILES["fileToUpload"]["name"]))["extension"]);
+			//$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+			//tekitame failinime koos ajatempliga
+			//$target_file = $target_dir ."hmv_" .(microtime(1) * 10000) ."." .$imageFileType;
+			//$imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+			
+			$target_file =($_SESSION["userId"]) ."." .$imageFileType;
+			
+			$check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+			if($check !== false) {
+				$notice .= "Fail on pilt - " . $check["mime"] . ". ";
+				$uploadOk = 1;
+			} else {
+				$notice .= "See pole pildifail. ";
+				$uploadOk = 0;
+			}
+			
+			//Kas selline pilt on juba üles laetud
+			if (file_exists($target_file)) {
+				$notice .= "Kahjuks on selle nimega pilt juba olemas. ";
+				$uploadOk = 0;
+			}
+			
+			//Piirame faili suuruse
+			if ($_FILES["fileToUpload"]["size"] > 1000000) {
+				$notice .= "Pilt on liiga suur! ";
+				$uploadOk = 0;
+			}
+			
+			//Piirame failitüüpe
+			if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg" && $imageFileType != "gif" ) {
+				$notice .= "Vabandust, vaid jpg, jpeg, png ja gif failid on lubatud! ";
+				$uploadOk = 0;
+			}
+			
+
+			if ($uploadOk == 0) {
+				$notice .= "Vabandust, pilti ei laetud üles! ";
+			//Kui saab üles laadida
+			} else {
+
+				//kasutan klassi
+				$myPhoto = new Photoupload($_FILES["fileToUpload"]["tmp_name"], $imageFileType);
+				$myPhoto->readExif();
+				$myPhoto->resizeImage($maxWidth, $maxHeight);
+				//$myPhoto->addWatermark();
+				//$myPhoto->addTextWatermark($myPhoto->exifToImage);
+				//$myPhoto->addTextWatermark("hmv_foto");
+				$myPhoto->savePhoto($target_dir, $target_file);
+				$myPhoto->clearImages();
+				unset ($myPhoto);
+				
+			}
+		
+		} else {
+			$notice = "Palun valige kõigepealt pildifail!";
+		} //kas üldse mõni fail valiti, lõppeb
+	}//kas vajutati submit nuppu, lõppeb
+	
+	
+
+	
+	
+	
+	
+	
+	
+	
+?>
+
+
+
+
+
+<?php require ("header.php")?>
+<h2>Siin on võimalik enda kasutajakontot redigeerida</h2>
+<h2>Foto üleslaadimine</h2>
+	<form action="profiiliredigeerimine.php" method="post" enctype="multipart/form-data">
+		<label>Valige pildifail:</label>
+		<input type="file" name="fileToUpload" id="fileToUpload">
+		<input type="submit" value="Lae üles" name="submit" id="submitPhoto"><span id="fileSizeError"></span>
+	</form>
+
+
+<?php require("footer.php") ?>
